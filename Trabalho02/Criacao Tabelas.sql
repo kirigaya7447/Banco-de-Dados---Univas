44d5c07aa516f55c1bae80538fee071429664d55
kirigaya7447 <joaoh2915@gmail.com>
2026-06-01 22:00:21 -0300

    Ajustando trabalho de 03/06/2026

--- Trabalho02/Criacao Tabelas.sql
+++ Trabalho02/Criacao Tabelas.sql
@@ -1,61 +1,61 @@
 create schema portal_estudantil;
 
 create table usuario(
-	codUsuario int,
-	login varchar(255),
-	senha varchar(255),
-	cargo varchar(255),
+	codUsuario int comment "identificador do usuario",
+	login varchar(255) comment "login do usuario",
+	senha varchar(255) comment "senha do usuario",
+	cargo varchar(255) comment "cargo do usuario",
 	primary key(codUsuario)
 );
 
 create table aluno(
-	codAluno int,
-	matriculaAluno int,
-	cpf varchar(20),
-	dataNasc date,
-	nome varchar(255),
-	codUsuario int,
+	codAluno int comment "identificador do aluno",
+	matriculaAluno int comment "matricula do aluno",
+	cpf varchar(20) comment "cpf do aluno",
+	dataNasc date comment "nascimento do aluno",
+	nome varchar(255) comment "nome do aluno",
+	codUsuario int comment "codigo do usuário do aluno",
 	primary key(codAluno)
 );
 
 create table curso(
-	codCurso int,
-	nome varchar(255),
-	codAluno int,
-	codProfessor int,
-	codMateria int,
+	codCurso int comment "",
+	nome varchar(255) comment "",
+	codAluno int comment "",
+	codProfessor int comment "",
+	codMateria int comment "",
 	primary key(codCurso)
 );
 
 create table professor(
-	codProfessor int,
-	dataNasc date,
-	matriculaProfessor int,
-	cpf varchar(20),
-	nome varchar(255),
-	codUsuario int,
+	codProfessor int comment "",
+	dataNasc date comment "",
+	matriculaProfessor int comment "",
+	cpf varchar(20) comment "",
+	nome varchar(255) comment "",
+	codUsuario int comment "",
 	primary key(codProfessor)
 );
 
 create table materia(
-	codMateria int,
-	horario time,
-	nome varchar(255),
-	sala varchar(255),
-	codProfessor int,
+	codMateria int comment "",
+	horario time comment "",
+	nome varchar(255) comment "",
+	sala varchar(255) comment "",
+	codProfessor int comment "",
 	primary key(codMateria)
 );
 
 create table professorCurso(
-	codProfessorCurso int,
-	codProfessor int,
-	codCurso int, 
+	codProfessorCurso int comment "",
+	codProfessor int comment "",
+	codCurso int comment "", 
 	primary key(codProfessorCurso)
 );
 
 create table CursoMateria(
-	codCursoMateria int,
-	codMateria int,
-	codCurso int,
+	codCursoMateria int comment "",
+	codMateria int comment "",
+	codCurso int comment "",
 	primary key(codCursoMateria)
 );
